8c30740b1d8972b6efc3d8c87ef05bd512102e82
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-18 00:18:46 -0800

    add commentRoutes.js

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const Comment = require('../../models');
+
+// route to create/add a comment using async/await
+router.post('/', async (req, res) => {
+  try { 
+    const commentData = await Comment.create({
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment,
+  });
+  // if the comment is successfully created, the new response will be returned as json
+  res.status(200).json(commentData)
+} catch (err) {
+  res.status(400).json(err);
+}
+});
+
+
+module.exports = router;
