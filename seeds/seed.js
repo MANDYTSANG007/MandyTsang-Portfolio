ee3d00df7a8b104ed20af12b92716fb336b65c0b
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-18 00:48:01 -0800

    modify seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,18 @@
 const sequelize = require('../config/connection');
-const {Project} = require('../models');
+const {Project , Comment} = require('../models');
 
 const projectData = require('./projectData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true});
 
     await Project.bulkCreate(projectData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Comment.bulkCreate(commentData, {
         individualHooks: true,
         returning: true,
     });
